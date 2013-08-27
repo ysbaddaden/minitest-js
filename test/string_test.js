af1b8a852c83f7562e48f451338a20fea6cfe3bb
Julien Portalier <julien@portalier.com>
2013-08-27 22:37:41 +0200

    Fixed inspect's recursion bug.

--- test/string_test.js
+++ test/string_test.js
@@ -2,23 +2,30 @@ describe("utils", function () {
     // FIXME: the minitest/utils module isn't exposed to the browser yet.
     if (typeof require === 'undefined') return;
     var assert = require('../lib/minitest').assert;
+    var utils = require('../lib/minitest/utils');
+
+    describe("inspect", function () {
+        it("won't inspect objects recursively", function () {
+            var A = function (b) { this.b = b; };
+            var B = function () { this.a = new A(this); };
+            assert.equal("{a: {b: ...}}", utils.inspect(new B()));
+        });
+    });
 
     describe("interpolate", function () {
-        var interpolate = require('../lib/minitest/utils').interpolate;
-
         it("must interpolate one key", function () {
             assert.equal("'interpolated' string",
-                interpolate("%{key} string", { key: "interpolated" }));
+                utils.interpolate("%{key} string", { key: "interpolated" }));
         });
 
         it("must interpolate many keys", function () {
             assert.equal("true expected to === false",
-                interpolate("%{exp} expected to === %{act}", { exp: true, act: false }));
+                utils.interpolate("%{exp} expected to === %{act}", { exp: true, act: false }));
         });
 
         it("must interpolate many times", function () {
             assert.equal("'a' 'b' 'a' 'a'",
-                interpolate("%{a} %{b} %{a} %{a}", { a: "a", b: "b" }));
+                utils.interpolate("%{a} %{b} %{a} %{a}", { a: "a", b: "b" }));
         });
     });
 });
