0092260a6ff5da9d6e03fd7a4638922f5d13ff61
Julien Portalier <julien@portalier.com>
2013-09-01 00:16:47 +0200

    MT_NO_EXPECTATIONS variable to skip the infection.

--- minitest.js
+++ minitest.js
@@ -1,5 +1,6 @@
 (function(){var global = this;function debug(){return debug};function require(p, parent){ var path = require.resolve(p) , mod = require.modules[path]; if (!mod) throw new Error('failed to require "' + p + '" from ' + parent); if (!mod.exports) { mod.exports = {}; mod.call(mod.exports, mod, mod.exports, require.relative(path), global); } return mod.exports;}require.modules = {};require.resolve = function(path){ var orig = path , reg = path + '.js' , index = path + '/index.js'; return require.modules[reg] && reg || require.modules[index] && index || orig;};require.register = function(path, fn){ require.modules[path] = fn;};require.relative = function(parent) { return function(p){ if ('debug' == p) return debug; if ('.' != p.charAt(0)) return require(p); var path = parent.split('/') , segs = p.split('/'); path.pop(); for (var i = 0; i < segs.length; i++) { var seg = segs[i]; if ('..' == seg) path.pop(); else if ('.' != seg) path.push(seg); } return require(path.join('/'), parent); };};require.register("minitest.js", function(module, exports, require, global){
 var Assertions = require('./minitest/assertions');
+require('./minitest/spec');
 
 module.exports = {
     AssertionError: Assertions.AssertionError,
@@ -394,9 +395,9 @@ module.exports = Mock;
 });require.register("minitest/spec.js", function(module, exports, require, global){
 var Expectations = require('./expectations');
 
-//if (typeof MT_NO_EXPECTATIONS === 'undefined' || !MT_NO_EXPECTATIONS) {
+if (typeof MT_NO_EXPECTATIONS === 'undefined' || !MT_NO_EXPECTATIONS) {
     Expectations.infect(Object.prototype);
-//}
+}
 
 });require.register("minitest/stub.js", function(module, exports, require, global){
 var utils = require('./utils');
@@ -608,8 +609,7 @@ module.exports = {
     message: message,
     inspect: inspect,
 
-    infectMethod: infectMethod,
-    supportsDefineProperty: supportsDefineProperty
+    infectMethod: infectMethod
 };
 
 });var exp = require('minitest');if ("undefined" != typeof module) module.exports = exp;else minitest = exp;
