9e64c3191af2ba46daf246f4600de4245b735312
Julien Portalier <julien@portalier.com>
2013-09-01 00:51:34 +0200

    Fixed matchers, thanks to some basic tests.

--- test/expectations_test.js
+++ test/expectations_test.js
@@ -6,22 +6,113 @@ var assert         = minitest.assert;
 describe("Expectations", function () {
     require('../lib/minitest/spec');
 
-    // skips legacy browsers that don't support properties
-    if (!Object.prototype.mustEqual) return;
+    describe("matchers", function () {
+        // skips legacy browsers that don't support properties
+        if (!Object.prototype.mustEqual) return;
 
-    it("must infect Object with expectations", function () {
-        assert.typeOf('function', Object.prototype.mustEqual);
-        assert.typeOf('function', Object.prototype.wontEqual);
-    });
+        it("must infect Object with expectations", function () {
+            assert.typeOf('function', Object.prototype.mustEqual);
+            assert.typeOf('function', Object.prototype.wontEqual);
+        });
 
-    it("mustEqual", function () {
-        "content".mustEqual("content");
-        assert.throws(AssertionError, function () { (1).mustEqual(2); });
-    });
+        it("mustBeEmpty", function () {
+            [].mustBeEmpty();
+            assert.throws(AssertionError, function () { ([1]).mustBeEmpty(); });
+        });
 
-    it("wontEqual", function () {
-        "content".wontEqual("<html>");
-        assert.throws(AssertionError, function () { (1).wontEqual(1); });
+        it("wontBeEmpty", function () {
+            [1].wontBeEmpty();
+            assert.throws(AssertionError, function () { ([]).wontBeEmpty(); });
+        });
+
+        it("mustEqual", function () {
+            "content".mustEqual("content");
+            assert.throws(AssertionError, function () { (1).mustEqual(2); });
+        });
+
+        it("wontEqual", function () {
+            "content".wontEqual("<html>");
+            assert.throws(AssertionError, function () { (1).wontEqual(1); });
+        });
+
+        it("mustBeWithinDelta", function () {
+            (0).mustBeWithinDelta(1.0 / 1000);
+            assert.throws(AssertionError, function () { (0).mustBeWithinDelta(1.0 / 999); });
+        });
+
+        it("wontBeWithinDelta", function () {
+            (0).wontBeWithinDelta(1.0 / 1000, 0.000001);
+            assert.throws(AssertionError, function () { (0).wontBeWithinDelta(1.0 / 1000, 0.1); });
+        });
+
+        it("mustBeWithinEpsilon", function () {
+            (9991).mustBeWithinEpsilon(10000);
+            (9999.1).mustBeWithinEpsilon(10000, 0.0001);
+            assert.throws(AssertionError, function () { (9990).mustBeWithinEpsilon(10000); });
+        });
+
+        it("wontBeWithinEpsilon", function () {
+            (9990 - 1).wontBeWithinEpsilon(10000);
+            assert.throws(AssertionError, function () { (9990).wontBeWithinEpsilon(10000); });
+        });
+
+        it("mustInclude", function () {
+            [1, 2].mustInclude(2);
+            assert.throws(AssertionError, function () { [1, 2].mustInclude(3); });
+        });
+
+        it("wontInclude", function () {
+            [1, 2].wontInclude(3);
+            assert.throws(AssertionError, function () { [1, 2].wontInclude(2); });
+        });
+
+        it("mustBeInstanceOf", function () {
+            [].mustBeInstanceOf(Array);
+            assert.throws(AssertionError, function () { [].mustBeInstanceOf(Number); });
+        });
+
+        it("wontBeInstanceOf", function () {
+            [].wontBeInstanceOf(Number);
+            assert.throws(AssertionError, function () { [].wontBeInstanceOf(Array); });
+        });
+
+        it("mustBeTypeOf", function () {
+            [].mustBeTypeOf('array');
+            assert.throws(AssertionError, function () { [].mustBeTypeOf('number'); });
+        });
+
+        it("wontBeTypeOf", function () {
+            [].wontBeTypeOf('number');
+            assert.throws(AssertionError, function () { [].wontBeTypeOf('array'); });
+        });
+
+        it("mustMatch", function () {
+            "content".mustMatch(/\w+/);
+            assert.throws(AssertionError, function () { "content".mustMatch(/\s+/); });
+        });
+
+        it("wontMatch", function () {
+            "content".wontMatch(/\s+/);
+            assert.throws(AssertionError, function () { "content".wontMatch(/\w+/); });
+        });
+
+        it("mustBeSameAs", function () {
+            var obj = {};
+            obj.mustBeSameAs(obj);
+            assert.throws(AssertionError, function () { ({}).mustBeSameAs(obj); });
+        });
+
+        it("wontBeSameAs", function () {
+            var obj = [];
+            [].wontBeSameAs(obj);
+            assert.throws(AssertionError, function () { obj.wontBeSameAs(obj); });
+        });
+
+        it("mustThrow", function () {
+            (function () { throw new Error(); }).mustThrow();
+            var e = (function () { throw new Error(); }).mustThrow(Error);
+            e.mustBeInstanceOf(Error);
+        });
     });
 });
 
