325cada5dfbd72ae3e5f5dbccfe7fa4d3ad71e37
Julien Portalier <julien@portalier.com>
2013-12-29 21:19:18 +0100

    First pass at spies.
    Spies are capable to create a new function, using `spy(name)`, or on
    an object, using `obj.spy(name)` instead. The spies can be verified
    to have been called, using `verify()`, and that it was called with a
    given list of arguments, using `verify(arg1, arg2)`. Arguments can
    also be loosely specified, like for mocks, using `verify(Number,
    Object)` for example.
    
    There are no assertions nor expectations for spies yet.

---
+++ test/spy_test.js
@@ -0,0 +1,70 @@
+var minitest = require('../lib/minitest');
+var assert = minitest.assert;
+var refute = minitest.refute;
+
+var spy = require('../lib/minitest/spy');
+var Mock = require('../lib/minitest/mock');
+
+describe("spy", function () {
+    var fn;
+
+    beforeEach(function () {
+        fn = spy('foo');
+    });
+
+    it("must create a spy", function () {
+        assert.typeOf('function', fn);
+        assert.typeOf('function', fn.verify);
+    });
+
+    it("won't verify when not called", function () {
+        refute(fn.verify());
+    });
+
+    it("must verify when called", function () {
+        fn();
+        assert(fn.verify());
+    });
+
+    it("must verify without checking parameters", function () {
+        fn(1, []);
+        assert(fn.verify());
+    });
+
+    it("must verify arguments", function () {
+        fn(1, '2');
+        assert(fn.verify(1, '2'));
+    });
+
+    it("won't verify with wrong arguments", function () {
+        fn(1, '2');
+        refute(fn.verify(1));
+        refute(fn.verify(2, '1'));
+    });
+
+    it("must verify with loosely specified arguments", function () {
+        fn = spy('loose_expectation');
+        fn(1, "", {}, []);
+        assert(fn.verify(Number, String, Object, Array));
+    });
+});
+
+describe(".spy method", function () {
+    var fn, obj = {};
+
+    beforeEach(function () {
+        fn = obj.spy('foo');
+    });
+
+    it("must create a spy on object", function () {
+        assert.same(fn, obj.foo);
+        assert.typeOf('function', obj.foo);
+        assert.typeOf('function', obj.foo.verify);
+    });
+
+    it("must verify", function () {
+        refute(obj.foo.verify());
+        obj.foo();
+        assert(obj.foo.verify());
+    });
+});
